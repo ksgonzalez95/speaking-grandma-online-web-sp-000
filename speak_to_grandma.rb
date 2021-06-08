c3762f3c3f2e56e28444d2e24e23876a0948a9a5
Kimberly Gonzalez <ksgonzalez95@gmail.com>
2021-06-08 01:45:30 +0000

    Automatically backed up by Learn

--- speak_to_grandma.rb
+++ speak_to_grandma.rb
@@ -1,5 +1,5 @@
 def speak_to_grandma(phrase)
-         if phrase == "I LOVE YOU GRANDMA!"
+         if phrase == "I LOVE YOU GRANDMA!".upcase
 				        "I LOVE YOU TOO PUMPKIN"
 				 elsif phrase != phrase.upcase
 				        "HUH!? SPEAK UP SONNY!"
